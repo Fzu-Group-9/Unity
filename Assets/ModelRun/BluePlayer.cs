ca6d311dd3e59983e372244adf8bc99a31c883c3
Minam1ya <2376786803@qq.com>
2023-11-17 20:03:35 +0800

    V1.0

---
+++ Assets/ModelRun/BluePlayer.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePlayer : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject BodyCollider;
+    public static bool alive;
+    public int canJump = 2;
+    public float jumpForce = 18f;
+    public float jumpDown = 40f;
+    public bool isJumpPressed, canDash, isDashing;
+    public Animator myAnim;
+    private Vector3 originPosition;
+    Rigidbody2D myRigi;
+    public AudioClip [] myAudioClip;
+    AudioSource myAudioSource;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+        myAnim = GetComponent<Animator>();
+        myRigi = GetComponent<Rigidbody2D>();
+        originPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        alive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!alive)
+        {
+            return;
+        }
+        originPosition = new Vector3(originPosition.x, transform.position.y, transform.position.z);
+        if (transform.position.x < originPosition.x) 
+        {
+            if (originPosition.x - transform.position.x > 10f) 
+            {
+                myAnim.SetTrigger("Die");
+                alive = false;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, originPosition, 1 * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad2) && canJump > 0)  
+        {
+            myAudioSource.PlayOneShot(myAudioClip[0]);
+            canDash = false;
+            if (canJump == 2)
+            {
+                isJumpPressed = true;
+            }
+            else if (canJump == 1)
+            {
+                myRigi.velocity = Vector2.up * 16;
+            }
+            canJump--;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3) && !canDash) 
+        {
+            myRigi.velocity = Vector2.down * jumpDown;
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad3) && canDash)
+        {
+            myAnim.SetBool("Slide",true);
+            canDash = false;
+            isDashing = true;
+            SetColliderOff();
+        }
+        if (Input.GetKeyUp(KeyCode.Keypad3))
+        {
+            myAnim.SetBool("Slide", false);
+            canDash = true;
+            isDashing = false;
+            SetColliderOn();
+        }
+    }
+        public void level_pause()
+    {
+        Time.timeScale=0;
+        menu.SetActive(true);
+    }
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        if (isJumpPressed) 
+        {
+                myRigi.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            
+            isJumpPressed = false;
+            myAnim.SetBool("Jump", true);
+        }
+    }
+    public void SetColliderOn()
+    {
+        BodyCollider.SetActive(true);
+    }
+    public void SetColliderOff()
+    {
+        BodyCollider.SetActive(false);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Trap")
+        {
+            myAnim.SetTrigger("Die");
+            alive = false;
+        }
+    }
+}
